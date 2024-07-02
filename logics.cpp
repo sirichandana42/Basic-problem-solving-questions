4eb678564fe115058f540a03fee02d29c5a1a6b7
Siri Chandana Gunturu <110028208+sirichandana42@users.noreply.github.com>
2024-07-02 17:48:51 +0530

    Update logics.cpp

--- logics.cpp
+++ logics.cpp
@@ -204,4 +204,17 @@ int main()
     }
 }
 
+//swapping w/o third variable
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<a<<" "<<b;
+    return 0;
+}
